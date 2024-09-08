4dc8c72a824728bdad6f77bc723e1d2d67f1ecf1
Tony Dinarte <dinartux@gmail.com>
2024-09-08 12:07:44 -0600

    project init

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import type { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'calculator',
+    loadComponent: () => import('./calculator/views/calculator-view/calculator-view.component').then(m => m.CalculatorViewComponent)
+  }
+];
